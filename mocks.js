f1954e554ec2022926ad488bf6337502220a1737
tim-zh <tim.zhlobich@gmail.com>
2016-11-13 22:34:34 +0300

    message

--- mocks.js
+++ mocks.js
@@ -3,7 +3,11 @@ $.mockjax({
   responseText: ["/home/123/code/"]
 });
 $.mockjax({
-  url: "/api/project",
+  url: "/api/project/new",
+  responseText: []
+});
+$.mockjax({
+  url: "/api/project/copy",
   responseText: []
 });
 $.mockjax({
@@ -78,4 +82,47 @@ $.mockjax({
       ]
     }
   }
+});
+$.mockjax({
+  url: "/api/examples",
+  responseText: ["build-info-example", "dotty-example", "multi-project-example", "resources-example", "scalafmt-example"]
+});
+$.mockjax({
+  url: "/api/example/files",
+  responseText: {
+    "name": "aaa",
+    "path": "",
+    "children": [
+      {
+        "name": "bbb",
+        "path": ""
+      },
+      {
+        "name": "asdfasdfafd",
+        "path": "",
+        "children": [
+          {
+            "name": "cccccc1",
+            "path": ""
+          },
+          {
+            "name": "cccccc2",
+            "path": ""
+          },
+          {
+            "name": "cccccc3",
+            "path": ""
+          },
+          {
+            "name": "cccccc4",
+            "path": ""
+          }
+        ]
+      }
+    ]
+  }
+});
+$.mockjax({
+  url: "/api/example/file",
+  responseText: "asdasfafasdfasdfa\nasdagsghsd ahdfli ashasdf hahsdklfuh a\na;ldsfhalk hasih dd fhdkuffvh \n\n\n\n\n\n\n\n\n\na;sdlkfj"
 });
